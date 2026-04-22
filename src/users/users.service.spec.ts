0721ce083c8a19b847e538da90a04fa322cdf893
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-04-22 11:09:10 +0200

    user services updated

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,30 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule, } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
+import { before, beforeEach, describe, } from 'node:test';
+
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repo: {
+    findOne: jest.Mock;
+    create: jest.Mock;
+    save: jest.Mock;
+  };
 
-  beforeEach(async () => {
+  beforeEach(async() => {
+    const repoMock = { 
+      findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
-        {
-          provide: getRepositoryToken(User),
-          useValue: {
-            findOne: jest.fn(),
-            create: jest.fn(),
-            save: jest.fn(),
-          },
-        },
+        { provide: getRepositoryToken(User),  useValue: repoMock},
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repo = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('createUser should return a safe user without password', async () => {
+    const dto = {name: 'Albert', email: 'albert@example.com', password: '123456'};
+
+  }
+
 });
