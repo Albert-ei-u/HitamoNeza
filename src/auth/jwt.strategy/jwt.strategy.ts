943e92763c5d5693aff1a5c6ed843e564ca6192b
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-04-22 07:25:22 +0200

    jwt strategy standardized

--- src/auth/jwt.strategy/jwt.strategy.ts
+++ src/auth/jwt.strategy/jwt.strategy.ts
@@ -11,6 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             secretOrKey: config.get<string>('JWT_SECRET'),
         });
     }  
+    
     async validate(payload: any) {
         return { userId: payload.sub, email: payload.email}
     } 
